af803fa36fe317726685605ac05519606077bad7
Md. pieash ahmed <81944951+pieash9@users.noreply.github.com>
2024-04-05 23:10:09 +0600

    [feat]: created login page and add shadcn UI

---
+++ src/app/(auth)/login/page.tsx
@@ -0,0 +1,108 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { SubmitHandler, useForm } from "react-hook-form";
+import * as z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { FormSchema } from "@/lib/types";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormMessage,
+} from "@/components/ui/form";
+import Link from "next/link";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import Loader from "@/components/global/Loader";
+
+const LoginPage = () => {
+  const router = useRouter();
+  const [submitError, setSubmitError] = useState("");
+
+  const form = useForm<z.infer<typeof FormSchema>>({
+    mode: "onChange",
+    resolver: zodResolver(FormSchema),
+    defaultValues: { email: "", password: "" },
+  });
+
+  const isLoading = form.formState.isSubmitting;
+
+  const onSubmit: SubmitHandler<z.infer<typeof FormSchema>> = async (
+    formData
+  ) => {
+    console.log(formData);
+    router.replace("/");
+  };
+  return (
+    <Form {...form}>
+      <form
+        onChange={() => {
+          if (submitError) setSubmitError("");
+        }}
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="w-full flex flex-col justify-center h-screen items-center sm:w-[400px] space-y-6  mx-auto"
+      >
+        <Link href="/" className="w-full flex items-center justify-center">
+          <span className="font-semibold dark:text-white text-4xl ml-2">
+            Shop Sphere
+          </span>
+        </Link>
+        <FormDescription className="text-foreground/60">
+          Welcome to Shop Sphere
+        </FormDescription>
+        <FormField
+          disabled={isLoading}
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormControl>
+                <Input
+                  className="sm:w-[400px]"
+                  type="email"
+                  placeholder="Email"
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          disabled={isLoading}
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem>
+              <FormControl>
+                <Input
+                  className="sm:w-[400px]"
+                  type="password"
+                  placeholder="Password"
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        {submitError && <FormMessage>{submitError}</FormMessage>}
+        <Button type="submit" className="w-full p-6" disabled={isLoading}>
+          {!isLoading ? "Login" : <Loader />}
+        </Button>
+        <span className="self-center">
+          Don{"'"}t have an account?{" "}
+          <Link className="text-sky-500 hover:underline " href="/signup">
+            Sign Up
+          </Link>
+        </span>
+      </form>
+    </Form>
+  );
+};
+
+export default LoginPage;
